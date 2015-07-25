6f16389ac94e34c2a2b0572edb5bb83c76ba2070
Patrick Best <patrickbest@patrickbest.com>
2015-07-25 21:12:03 +0100

    add menu with js and styling

--- js/main.js
+++ js/main.js
@@ -6,8 +6,52 @@ function l(data) {
   console.log(data);
 }
 
+var mainContent = $('#main-content');
+var scrollTop;
+
+var Menu = {
+  $menuHolder: $('#menu-holder'),
+  menuTop: 0,
+  setup: function() {
+    Menu.menuTop = Menu.$menuHolder.offset().top;
+  },
+  getHeight: function() {
+    var _this = this;
+    var height = _this.$menuHolder.height();
+
+    return height;
+  },
+  fix: function() {
+    var _this = this;
+
+    _this.$menuHolder.addClass('u-fixed');
+    mainContent.css('padding-top', _this.getHeight() + 'px');
+  },
+  unFix: function() {
+    var _this = this;
+
+    _this.$menuHolder.removeClass('u-fixed');
+    mainContent.css('padding-top', '0px');
+  },
+  fixLogic: function(scrollTop) {
+    var _this = this;
+
+    if (_this.$menuHolder.hasClass('u-fixed') && scrollTop <= _this.menuTop) {
+      _this.unFix();
+    } else if (scrollTop >= _this.menuTop) {
+      _this.fix();
+    }
+  },
+}
+
 jQuery(document).ready(function () {
   'use strict';
+  Menu.setup();
+
+  $(window).on('scroll', function(e) {
+    scrollTop = $(window).scrollTop();
+    Menu.fixLogic(scrollTop);
+  });
 
   $('.js-scroll-to-trigger').on('click', function(e) {
     e.preventDefault();
--- partials/menu.php
+++ partials/menu.php
@@ -1,11 +1,13 @@
-<nav id="menu" class="container">
+<div id="menu-holder" class="yellow-background">
+  <nav id="menu" class="container">
 
-  Silke
+    <div id="menu-logo"><?php echo url_get_contents(get_stylesheet_directory_uri() . '/img/silke_logo.svg'); ?></div>
 
-  <ul>
-    <li class="js-scroll-to-trigger u-pointer" data-target="us">Us</li>
-    <li class="js-scroll-to-trigger u-pointer" data-target="work">Work</li>
-    <li class="js-scroll-to-trigger u-pointer" data-target="contact">Contact</li>
-  </ul>
+    <ul id="menu-list" class="font-bold">
+      <li class="js-scroll-to-trigger u-pointer" data-target="us">Us</li>
+      <li class="js-scroll-to-trigger u-pointer" data-target="work">Work</li>
+      <li class="js-scroll-to-trigger u-pointer" data-target="contact">Contact</li>
+    </ul>
 
-</nav>
+  </nav>
+</div>
